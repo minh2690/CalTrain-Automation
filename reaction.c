c6bb6e0b08e016ce50a582222046948f07425e1f
MINH1622 <huytranngoc098@gmail.com>
2021-05-14 14:22:14 +0700

    update reaction

--- reaction.c
+++ reaction.c
@@ -6,26 +6,43 @@
 void make_water();
 
 struct reaction {
-	int h, o;
+	int h;
 
 	struct lock lock;
 	struct condition h_full;
+	struct condition wait_done;
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+	reaction->h = 0;
+	lock_init(&reaction->lock);
+	cond_init(&reaction->h_full);
+	cond_init(&reaction->wait_done);
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	reaction->h ++;
+	cond_signal(&reaction->h_full, &reaction->lock);
+	cond_wait(&reaction->wait_done, &reaction->lock);
+	lock_release(&reaction->lock);
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
-	// FILL ME IN
+	lock_acquire(&reaction->lock);
+	while (reaction->h < 2)
+	{
+		cond_wait(&reaction->h_full, &reaction->lock);
+	}
+	reaction->h -= 2;
+	make_water();
+	cond_signal(&reaction->wait_done, &reaction->lock);
+	cond_signal(&reaction->wait_done, &reaction->lock);
+	lock_release(&reaction->lock);
 }
